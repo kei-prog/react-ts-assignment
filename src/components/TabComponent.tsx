606fbc5a71ae4e8233d6caea8384bd24e01f95dc
kei-prog <kei08prog@gmail.com>
2025-02-05 16:37:13 +0900

    不要な<></>削除

--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -22,44 +22,42 @@ export const TabComponent = ({ handleClick }: Props) => {
   };
 
   return (
-    <>
-      <div>
-        <ul className="nav nav-pills">
-          <li className="nav-item">
-            <a
-              data-tab="all"
-              className={`nav-link ${activeTab === "all" ? "active" : ""}`}
-              aria-current={activeTab === "all" ? "page" : undefined}
-              href="#"
-              onClick={onClick}
-            >
-              全員
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              data-tab="students"
-              className={`nav-link ${activeTab === "students" ? "active" : ""}`}
-              aria-current={activeTab === "students" ? "page" : undefined}
-              href="#"
-              onClick={onClick}
-            >
-              生徒
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              data-tab="mentors"
-              className={`nav-link ${activeTab === "mentors" ? "active" : ""}`}
-              aria-current={activeTab === "mentors" ? "page" : undefined}
-              href="#"
-              onClick={onClick}
-            >
-              メンター
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a
+            data-tab="all"
+            className={`nav-link ${activeTab === "all" ? "active" : ""}`}
+            aria-current={activeTab === "all" ? "page" : undefined}
+            href="#"
+            onClick={onClick}
+          >
+            全員
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            data-tab="students"
+            className={`nav-link ${activeTab === "students" ? "active" : ""}`}
+            aria-current={activeTab === "students" ? "page" : undefined}
+            href="#"
+            onClick={onClick}
+          >
+            生徒
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            data-tab="mentors"
+            className={`nav-link ${activeTab === "mentors" ? "active" : ""}`}
+            aria-current={activeTab === "mentors" ? "page" : undefined}
+            href="#"
+            onClick={onClick}
+          >
+            メンター
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
